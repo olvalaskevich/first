1ba0f370d5fedb104982da717534517928dfbbea
olvalaskevich <olvol269@gmail.com>
2023-10-10 19:35:57 +0300

    add useState

--- src/components/OnOff.tsx
+++ src/components/OnOff.tsx
@@ -1,42 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../index.css'
+import {Btn} from "./Btn";
 
 
-type BtnPropsType={
-    state:string
-    name?:string
+
+
+
+
+
+export const OnOff = () => {
+
+let [on, setOn]=useState(false)
+
+function tumbler(){
+    setOn(!on)
 }
 
-type OnOffPropsType={
-    state:boolean,
-
-}
-
-
-export const OnOff = (props:OnOffPropsType) => {
     return (
         <div>
 
-            {props.state &&
+            {on &&
                 <div>
-                    <Btn state='On' name='on'/>
-                    <Btn state='Off'/>
+                    <Btn name='on' title={'On'}/>
+                    <Btn tumbler={tumbler} title={'Off'}/>
+                    <img src='https://avatars.mds.yandex.net/get-mpic/4120716/img_id6365070089787389708.jpeg/9' alt='on'/>
                 </div> ||
                 <div>
-                    <Btn state='On'/>
-                    <Btn state='Off' name='off'/>
+                    <Btn tumbler={tumbler} title={'On'}/>
+                    <Btn name='off' title={'Off'}/>
+                    <img src='https://avatars.mds.yandex.net/i?id=2e5ac2b9020e562f100bf5549029665709f99062-10767321-images-thumbs&n=13' alt='off'/>
                 </div>}
-
-            {props.state &&
-                <img src='https://avatars.mds.yandex.net/get-mpic/4120716/img_id6365070089787389708.jpeg/9' alt='on'/> ||
-                <img src='https://avatars.mds.yandex.net/i?id=2e5ac2b9020e562f100bf5549029665709f99062-10767321-images-thumbs&n=13' alt='off'/>
-            }
-
 
         </div>
     );
 };
 
-const Btn = (props:BtnPropsType)=> {
-    return <button className={props.name}>{props.state}</button>
-}
